766598cda9e555dae3fdb4d4c597d5f0578bbc62
123Brijesh44aa <oddsworld0121@gmail.com>
2025-05-07 21:30:32 +0530

    modified : isPowerOfTwo.java -> added more optimized code

--- src/recursions/basics/IsPowerOfTwo.java
+++ src/recursions/basics/IsPowerOfTwo.java
@@ -12,7 +12,13 @@ public class IsPowerOfTwo {
         return isPowerOfTwo(n/2);
     }
 
+    static boolean isPowerOfTwo_optimized(int n){
+        if (n<=0){return false; }
+        return Integer.bitCount(n) == 1;
+    }
+
     public static void main(String[] args) {
         System.out.println(isPowerOfTwo(16));
+        System.out.println(isPowerOfTwo_optimized(1024));
     }
 }
