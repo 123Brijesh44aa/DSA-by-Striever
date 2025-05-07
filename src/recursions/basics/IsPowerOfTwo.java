e3ec62b6cc2c0a3a41c49b90670bbfbb009eeaf3
123Brijesh44aa <oddsworld0121@gmail.com>
2025-05-07 19:01:31 +0530

    new file:   src/recursions/basics/IsPowerOfTwo.java

---
+++ src/recursions/basics/IsPowerOfTwo.java
@@ -0,0 +1,18 @@
+package recursions.basics;
+
+public class IsPowerOfTwo {
+
+    static boolean isPowerOfTwo(int n){
+        if (n==1){
+            return true;
+        }
+        if (n<=0 || n%2 != 0){
+            return false;
+        }
+        return isPowerOfTwo(n/2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(16));
+    }
+}
