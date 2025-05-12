d52e36e461df0e9e99d4338581a4a0ddd5948f8b
123Brijesh44aa <oddsworld0121@gmail.com>
2025-05-12 14:33:07 +0530

    modified: ShiftArrayElementsRightLeft.java

--- src/arrays/basics/ShiftArrayElementsRightLeft.java
+++ src/arrays/basics/ShiftArrayElementsRightLeft.java
@@ -2,7 +2,7 @@ package arrays.basics;
 
 import java.util.Arrays;
 
-public class ShiftArrayElementsRightsideBy1 {
+public class ShiftArrayElementsRightLeft {
 
     static int[] shiftRightside_by_1(int[]arr){
         int temp = arr[arr.length-1];
@@ -13,8 +13,54 @@ public class ShiftArrayElementsRightsideBy1 {
         return arr;
     }
 
+    static int[] rightShiftByN(int[]arr,int shiftBy){
+
+        if (shiftBy == 0) return arr;
+
+        if (shiftBy < 0){
+           // Left shift
+            return leftShiftByN(arr, -shiftBy);
+        }
+
+        shiftBy = shiftBy % arr.length;
+
+        if (shiftBy == 0) return arr;
+
+        int[]temp = new int[shiftBy];
+        for (int i=0; i<shiftBy; i++){
+            temp[i] = arr[arr.length-shiftBy+i];
+        }
+        for (int j=arr.length-1; j>=shiftBy; j--){
+            arr[j] = arr[j-shiftBy];
+        }
+        for (int k=0; k<shiftBy; k++){
+            arr[k] = temp[k];
+        }
+        return arr;
+    }
+
+    static int[] leftShiftByN(int[]arr, int shiftBy){
+        if (shiftBy == 0) return arr;
+        if (shiftBy < 0){
+            return rightShiftByN(arr, -shiftBy);
+        }
+        shiftBy = shiftBy%arr.length;
+        if (shiftBy == 0) return arr;
+        int[]temp = new int[shiftBy];
+        for (int i=0; i<shiftBy; i++){
+            temp[i] = arr[i];
+        }
+        for (int j=0; j<arr.length-shiftBy; j++){
+            arr[j] = arr[shiftBy+j];
+        }
+        for (int k=0; k<shiftBy; k++){
+            arr[arr.length-shiftBy+k] = temp[k];
+        }
+        return arr;
+    }
+
     public static void main(String[] args) {
         int[]arr = {10,20,30,40,50,60};
-        System.out.println(Arrays.toString(shiftRightside_by_1(arr)));
+        System.out.println(Arrays.toString(rightShiftByN(arr,-98)));
     }
 }
