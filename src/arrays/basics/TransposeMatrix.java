4742a4d8763bbc6f4868d086c1016e976ffeee9c
123Brijesh44aa <oddsworld0121@gmail.com>
2025-05-12 22:54:58 +0530

    modified : TransposeMatrix.java

--- src/arrays/basics/TransposeMatrix.java
+++ src/arrays/basics/TransposeMatrix.java
@@ -16,11 +16,32 @@ public class TransposeMatrix {
         return transposedMatrix;
     }
 
+    // another method: by swaping row,column to column, row,
+    // but this method works for only square matrices.
+    // e.g., 3x3, 2x2, 5x5
+    // if you try to give it non-square matrix, you will end up getting IndexOutOfBoundException.
+    static int[][] transpose2(int[][]arr){
+        int rows = arr.length;
+        int cols = arr[0].length;
+        for (int i=0; i<rows; i++){
+            for (int j=i; j<cols; j++){
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+        return arr;
+    }
+
+
     public static void main(String[] args) {
         int[][]arr = {
-                {1,2,3},
-                {4,5,6},
-                {7,8,9}
+                {1,2,3,44,32,90},
+                {4,5,6,49,55,66},
+                {7,8,9,77,88,99},
+                {32,98,43,53,23,53},
+                {38,58,20,83,52,39},
+                {59,89,33,50,19,15}
         };
         System.out.println("Original Matrix");
         for (int i=0; i<arr.length; i++){
@@ -31,7 +52,7 @@ public class TransposeMatrix {
             System.out.println(row);
         }
         System.out.println("Transposed Matrix");
-        arr = transpose(arr);
+        arr = transpose2(arr);
         for (int i=0; i<arr.length; i++){
             StringBuilder row = new StringBuilder();
             for (int j=0; j<arr[0].length; j++){
