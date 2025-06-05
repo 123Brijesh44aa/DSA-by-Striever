dba07670fe2175d1f616f5ae5c7c07e5badb072f
123Brijesh44aa <oddsworld0121@gmail.com>
2025-06-05 22:21:50 +0530

    solved problem : addd 1 to a number represented by linked list

---
+++ src/Add1ToANumberRepresentedByLinkedList.java
@@ -0,0 +1,69 @@
+import linkedlist.leetcode.common.LinkedList;
+import linkedlist.leetcode.common.Node;
+
+public class Add1ToANumberRepresentedByLinkedList {
+
+    private static void reverseLinkedList(LinkedList linkedList){
+        Node next = null;
+        Node prev = null;
+        Node temp = linkedList.head;
+        while (temp != null) {
+            next = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = next;
+        }
+        linkedList.head = prev;
+    }
+
+
+    private static LinkedList add_1_to_a_number_represented_by_linkedList(LinkedList linkedList){
+        reverseLinkedList(linkedList);
+        Node temp = linkedList.head;
+        int carry = 1;
+        while (temp != null) {
+            temp.data = temp.data + carry;
+            if (temp.data < 10) {
+                carry = 0;
+                break;
+            } else{ //temp.data == 10
+                temp.data = 0;
+                carry = 1;
+            }
+            temp = temp.next;
+        }
+        if (carry == 1) {
+            Node newNode = new Node(1);
+            reverseLinkedList(linkedList);
+            newNode.next = linkedList.head;
+            linkedList.head = newNode;
+            return linkedList;
+        }
+        reverseLinkedList(linkedList);
+        return linkedList;
+    }
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insert(0, 1);
+        list.insert(1, 5);
+        list.insert(2, 9);
+        System.out.println("original list is : ");
+        list.printList();
+        list = add_1_to_a_number_represented_by_linkedList(list);
+        System.out.println("Linked list after adding 1 :");
+        list.printList();
+
+        System.out.println();
+
+        LinkedList list2 = new LinkedList();
+        list2.insert(0, 9);
+        list2.insert(1, 9);
+        list2.insert(2, 9);
+        list2.insert(3, 9);
+        System.out.println("original list2");
+        list2.printList();
+        System.out.println("list2 after adding 1");
+        list2 = add_1_to_a_number_represented_by_linkedList(list2);
+        list2.printList();
+    }
+}
