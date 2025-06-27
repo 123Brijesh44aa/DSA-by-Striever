9c05d7aa6f8ade3035b30600f3f06feecf7676bf
123Brijesh44aa <oddsworld0121@gmail.com>
2025-06-27 22:00:59 +0530

    modified 0/1 knapsack : solved it using bottom up approach

--- src/dp/ZeroOneKnapSack.java
+++ src/dp/ZeroOneKnapSack.java
@@ -1,5 +1,7 @@
 package dp;
 
+import java.util.Arrays;
+
 public class ZeroOneKnapSack {
 
     // Problem Statement:
@@ -33,11 +35,60 @@ public class ZeroOneKnapSack {
         }
     }
 
+    // memoization
+    private static int helper(int[]weight,int[]value,int w,int n,int[][]dp){
+        if (n == 0 || w == 0){
+            return 0;
+        }
+        if (dp[n][w] != -1){
+            return dp[n][w];
+        }
+        if (weight[n-1] > w){
+            return dp[n][w] = helper(weight,value,w,n-1,dp);
+        } else { // weight[n-1] <= w
+            return dp[n][w] = Math.max(
+                    value[n-1] + helper(weight,value,w-weight[n-1],n-1,dp),
+                    helper(weight,value,w,n-1,dp)
+            );
+        }
+    }
+    private static int maxProfit_memo(int[] weight, int[] value, int w, int n){
+        int [][] dp = new int[n+1][w+1];
+        for (int[] row : dp){
+            Arrays.fill(row, -1);
+        }
+        return helper(weight,value,w,n,dp);
+    }
+
+    // Bottom up approach
+    private static int maxProfit_optimal(int[] weight, int[] value, int w, int n){
+        int[][] dp = new int[n+1][w+1];
+        for (int i = 0; i<n+1; i++){
+            for (int j = 0; j<w+1; j++){
+                if (i == 0 || j == 0){
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i<n+1; i++){
+            for (int j = 1; j<w+1; j++){
+                if (weight[i-1] <= j) {
+                    dp[i][j] = Math.max(value[i-1] + dp[i-1][j-weight[i-1]], dp[i-1][j]);
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][w];
+    }
+
     public static void main(String[] args) {
         int[] wt = {1,3,4,5};
         int[] val = {1,4,5,7};
         int w = 7;
         int n = wt.length;
         System.out.println("max profit is : "+ maxProfit(wt,val,w,n));
+        System.out.println("max profit is (memo) : "+ maxProfit_memo(wt,val,w,n));
+        System.out.println("max profit is (bottom up) : "+ maxProfit_optimal(wt,val,w,n));
     }
 }
