5909347112120a4890ab490dde2886f63f8ab355
123Brijesh44aa <oddsworld0121@gmail.com>
2025-08-25 20:10:31 +0530

    renamed : Rotate Linked List by k

---
+++ src/RotateLinkedListByK.java
@@ -0,0 +1,45 @@
+import linkedlist.leetcode.common.LinkedList;
+import linkedlist.leetcode.common.Node;
+
+public class RotateLinkedListByK {
+
+    private static LinkedList rotateByK(LinkedList linkedList, int k) {
+        if (linkedList.head == null || linkedList.head.next == null) {
+            return linkedList;
+        }
+        Node temp = linkedList.head;
+        int length = 1;
+        while (temp.next != null) {
+            temp = temp.next;
+            length++;
+        }
+
+        Node tail = temp;
+
+        k = k % length;
+
+        if (k == 0) {
+            return linkedList;
+        }
+
+        temp = linkedList.head;
+
+        int steps = length - k;
+
+        for (int i = 0; i < steps - 1; i++) {
+            temp = temp.next;
+        }
+
+        Node next = temp.next;
+        temp.next = null;
+        tail.next = linkedList.head;
+        linkedList.head = next;
+
+        return linkedList;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
