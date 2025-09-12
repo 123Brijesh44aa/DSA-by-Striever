750bf38a4e137df326d9aac66dfd35961031b189
123Brijesh44aa <oddsworld0121@gmail.com>
2025-09-12 17:01:32 +0530

    solved : Leetcode 11 (Container with Most Water)

---
+++ src/neetcode150course/ContainerWithMostWater11.java
@@ -0,0 +1,55 @@
+public class ContainerWithMostWater11 {
+
+    // problem description :
+    // You are given an integer array height of length n. There are n vertical lines
+    // drawn such that the two endpoints of the ith line are (i, 0) and (i,
+    // height[i]).
+
+    // Find two lines that together with the x-axis form a container, such that the
+    // container contains the most water.
+
+    // Return the maximum amount of water a container can store.
+
+    // Notice that you may not slant the container.
+
+    // Example 1:
+
+    // Input: height = [1,8,6,2,5,4,8,3,7]
+    // Output: 49
+    // Explanation: The above vertical lines are represented by array
+    // [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the
+    // container can contain is 49.
+    // Example 2:
+
+    // Input: height = [1,1]
+    // Output: 1
+
+    // TWO POINTER SOLUTION
+    private static int mostWater(int[] nums) {
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
+        int mostWater = Integer.MIN_VALUE;
+        while (left < right) {
+            int width = right - left;
+            int height = Math.min(nums[left], nums[right]);
+            int area = width * height;
+            mostWater = Math.max(mostWater, area);
+            if (nums[left] < nums[right]) {
+                left++;
+            } else if (nums[left] > nums[right]) {
+                right--;
+            } else {
+                left++;
+                right--;
+            }
+        }
+        return mostWater;
+    }
+
+    public static void main(String[] args) {
+        int[] height = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+        height = new int[] { 1, 1 };
+        System.out.println("most water is : " + mostWater(height));
+    }
+}
